f0a5b78e5b108476edd50e1ef7c4aae5973e72df
UmerovEmran <99113996+UmerovEmran@users.noreply.github.com>
2022-02-13 21:49:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,41 +1,13 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-        int dl =0;
-
-   if(a == b) {
+    // поместить сюда текст реализации алгоритма (на С++)
+    while (a != b) {
+       if (a > b) {
+         a = a - b;
+       } else {
+         b = b - a;
+       }
+    }
     return a;
-   }
-   else if (a > b) {
-    if (0 == (a & b)) {
-        return b;
-    }
-    else {
-        dl = b;
-        for (; 0<dl; dl--) {
-            if (0 == (b % dl) && 0 == (a % dl)) {
-                return dl;
-                break;
-            }
-        }
-    }
-   }
-   else {
-    if (0 == (b & a)) {
-        return a;
-    }
-    else {
-        dl = a;
-        for (; 0<dl; dl--) {
-            if (0 == (a % dl) && 0 == (b % dl)) {
-                return dl;
-                break;
-            }
-        }
-    }
-   }
-   if ( 0 == dl) {
-    return 1;
-   }
-   return 0;     
 }
